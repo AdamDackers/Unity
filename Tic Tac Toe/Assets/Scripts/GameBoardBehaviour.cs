10d6cf8fdf3c61486e7fa5436338dce52e01683e
adam dackers <adam dackers>
2017-04-02 21:32:03 +1200

    fixed grid objects instantiating at the middle of the gameboard.

--- Tic Tac Toe/Assets/Scripts/GameBoardBehaviour.cs
+++ Tic Tac Toe/Assets/Scripts/GameBoardBehaviour.cs
@@ -8,8 +8,12 @@ public class GameBoardBehaviour : MonoBehaviour {
     private RectTransform gridPrefab;
     private RectTransform[] gridArray;
 
+    private RectTransform rt;
+
 	// Use this for initialization
 	void Start () {
+
+        rt = gameObject.GetComponent<RectTransform>();
         gridPrefab = Resources.Load("Prefabs/gridSquare",typeof(RectTransform)) as RectTransform;
         gridArray = new RectTransform[9];
         ResetPlaySpace();
@@ -21,18 +25,29 @@ public class GameBoardBehaviour : MonoBehaviour {
 
     private void ResetPlaySpace()
     {
-        Debug.Log("starting reset PlaySpace");
-        int spaceValue = 0;
+        float size = rt.rect.size.x / 3;
+        float spacingX = -size; //set x-1 size so that first loop run sets a 0
+        float spacingY = 0;
+
         for(int i=0; i < gridArray.Length;i++)
         {
             if(i % 3 == 0)
             {
-                spaceValue++;
+                spacingY = 0;
+                spacingX += size;
             }
+            else
+            {
+                spacingY -= size;
+            }
+
             gridArray[i] = Instantiate(gridPrefab);
+            gridArray[i].name = "grid" + i;
             gridArray[i].SetParent(gameObject.transform);
             gridArray[i].gameObject.transform.localScale = gameObject.transform.localScale;
-            //gridArray[i].gameObject.transform.Translate(gridArray[i].rect.width * spaceValue, gridArray[i].rect.height * spaceValue, gridArray[i].transform.transform.position.z);
+            gridArray[i].sizeDelta = new Vector2(size, size);
+            gridArray[i].gameObject.transform.localPosition = new Vector3(spacingX, spacingY, 0);
+
         }
         
 
